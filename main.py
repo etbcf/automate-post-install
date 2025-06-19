3fa565e3aea9fff18a1601208ba42734c261075f
etbcf <tarik.edu2@gmail.com>
2025-06-19 21:02:58 +0100

    Fixed infinite loop

--- main.py
+++ main.py
@@ -4,17 +4,9 @@ import os
 import subprocess
 import sys
 
-repo_url = "https://github.com/etbcf/automate-post-install.git"
 repo_name = "automate-post-install"
 
 try:
-    # Check if the repo directory already exists
-    if not os.path.isdir(repo_name):
-        print(f"Cloning repository '{repo_name}'...")
-        subprocess.run(["git", "clone", repo_url])
-    else:
-        print(f"Repository '{repo_name}' already exists. Skipping clone.")
-
     os.chdir(repo_name)
 
     # Run the main.py script
@@ -24,5 +16,6 @@ try:
 
 except subprocess.CalledProcessError as e:
     print(f"Command failed: {e}")
+
 except Exception as e:
     print(f"Error: {e}")
